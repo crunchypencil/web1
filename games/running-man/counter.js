d70ed36bca75333979940d359c1dc1e643a3ead2
crunchypencil <dcahill@crunchypencil.com>
2014-10-05 20:22:17 -0400

    update

--- games/running-man/counter.js
+++ games/running-man/counter.js
@@ -4,10 +4,11 @@ var j = "PISS!";
 function timedCount() {
     i = i - 1;
 	if (i<=0) {
-		postMessage(j)
+		postMessage(j);
+		clearTimeout(timey);
 	} else {
 		postMessage(i);
-		clearTimeout(timey);
+		
 	}
     var timey = setTimeout("timedCount()",1000);
 }
